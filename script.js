e89151ffb0950c41f974d2f90d113fbe9b54739a
SiddhiKanakiya <siddhikanakiya@gmail.com>
2022-12-20 11:17:15 +0530

    first commit

--- script.js
+++ script.js
@@ -1,81 +1,138 @@
-var body = document.getElementsByTagName("body")[0];
-var plays = document.getElementById("play");
-var turns = document.getElementById("game-number");
-var updategameturn = document.getElementById("turns")
-var updatePlayerScore = document.getElementById("player")
-var updateComputerScore = document.getElementById("computer")
+let body = document.getElementsByTagName("body")[0];
+let plays = document.getElementById("play");
+let turns = document.getElementById("game-number");
+let updategameturn = document.getElementById("turns");
+let updatePlayerScore = document.getElementById("player");
+let updateComputerScore = document.getElementById("computer");
+let continuePlaying = false;
 
-var playerselection =  "";
-var computerselection =  "";
-var playerscore = 0;
-var computerscore = 0;
+let playerselection =  "";
+let computerselection =  "";
+let playerscore = 0;
+let computerscore = 0;
+let turnNo = 0;
+// Updating the number of turns
+plays.onclick = play;
 
-
-plays.onclick = play
-
-function play()
-{
+function play(){
 playerselection =  "";
 computerselection =  "";
-var turnNo = Number(turns.value);
+turnNo = Number(turns.value);
 
 if(Number.isNaN(turnNo)){
     alert("Enter number again");
 }
 else{
+   continuePlaying = true;
    updategameturn.innerText = turnNo;
+   playerscore = 0;
+   computerscore = 0;
    updatePlayerScore.innerText = 0;
    updateComputerScore.innerText = 0;
 }
 }
 
-if( updategameturn.innerText == 0){
-    Finalwinner(updatePlayerScore,updateComputerScore);
-}
-
-
-
-
-
+// While the turns is more than zero
 var rockbutton = document.getElementById("rock");
 var paperbutton = document.getElementById("paper");
 var scissorsbutton = document.getElementById("scissors");
 
-// when you click rock button
 
-function  rockclicked(){
 
-    var playerselection =  "rock";
-    var computerselection = computer();
-    let winner = win(playerselection,computerselection);
-    let roundwin = document.getElementById("roundwinner");
-    roundwin.innerText = winner;
-    updateScore(winner);
+rockbutton.onclick = function(){
+   if(turnNo>0) { 
+     
+      playerselection="rock";
+      roundwinner();
+      turnNo--;
+      updategameturn.innerText = turnNo;  }
+   else if(continuePlaying==true){
+      gamewinner();
+      continuePlaying=false;
+   }
+   else return
 }
 
-rockbutton.onclick = rockclicked;
-// When you select paper
-function  paperclicked(){
+paperbutton.onclick = function(){
+   if(turnNo>0) {
 
-    var playerselection =  "paper";
-    var computerselection = computer();
-    let winner = win(playerselection,computerselection);
-    let roundwin = document.getElementById("roundwinner");
-    roundwin.innerText = winner;
-    updateScore(winner);
+      playerselection="paper";
+      roundwinner();
+      turnNo--;
+      updategameturn.innerText = turnNo;  }
+   else if(continuePlaying==true){
+         gamewinner();
+         continuePlaying=false;
+   }
+
+   else return;
 }
 
-paperbutton.onclick = paperclicked;
+scissorsbutton.onclick = function(){
+   if(turnNo>0) {
+      
+      playerselection="scissors";
+      roundwinner();
+      turnNo--;
+      updategameturn.innerText = turnNo;  }
 
-// When scissor is clicked
-function  scissorsclicked(){
-
-    var playerselection =  "scissor";
-    var computerselection = computer();
-    let winner = win(playerselection,computerselection);
-    let roundwin = document.getElementById("roundwinner");
-    roundwin.innerText = winner;
-    updateScore(winner);
+   else if(continuePlaying==true){
+         gamewinner();
+         continuePlaying=false;
+   }
+   else return;
 }
 
-scissorbutton.onclick = scissorclicked;
+
+function computer(){
+    
+let computerOptions = ["rock", "paper" , "scissors"]
+let choiceNumber = Math.floor(Math.random()*3);
+ computerselection = computerOptions[choiceNumber]
+}
+
+function roundwinner(){
+   computer();
+   let round = document.getElementById("roundwinner");
+   let computerchoose= document.getElementById("computerchoose");
+   computerchoose.innerText = "Computer Selected: " + computerselection;
+
+   
+   if((playerselection=="rock" && computerselection=="rock") || (playerselection=="paper" && computerselection=="paper") || (playerselection=="scissors" && computerselection=="scissors")){
+      round.innerText = "Tie"
+   }
+   else if((playerselection=="rock" && computerselection=="scissors") || (playerselection=="paper" && computerselection=="rock") || (playerselection=="scissors" && computerselection=="paper")){
+      playerscore ++;
+      updatePlayerScore.innerText = playerscore
+      round.innerText = "Player Won"
+  }
+  else{
+   round.innerText ="Computer Won"
+   computerscore++;
+   updateComputerScore.innerText =  computerscore;
+  }
+
+}
+
+
+function gamewinner(){
+   let game= document.getElementById("roundwinner");
+   if(playerscore>computerscore){
+      game.innerText = "Player won the game!";
+   }
+   else if(computerscore>playerscore){
+      game.innerText = "Computer won the game!";
+   }
+   else{
+      game.innerText = "It was tie";
+   }
+
+
+}
+
+
+
+
+
+
+//  
